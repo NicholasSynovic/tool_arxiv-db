4fd036dfa8ea4d920975042ef2f55a4c4dcb5da3
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-28 15:17:37 -0500

    Add docstrings

--- src/db.py
+++ src/db.py
@@ -15,12 +15,44 @@ from sqlalchemy import (
 
 
 class DB:
+    """
+    A database management class for handling connections and schema
+    definitions in a SQLite database.
+
+    This class manages the creation of an engine connected to the specified
+    SQLite database and defines the schema for documents and categories tables
+    using SQLAlchemy's ORM capabilities.
+
+    :param dbPath: The filesystem path to the SQLite database file.
+    :type dbPath: Path
+    """
+
     def __init__(self, dbPath: Path) -> None:
+        """
+        Initialize the DB class with a specific path to the SQLite database.
+
+        Sets up the SQLAlchemy engine and binds metadata for schema definition.
+
+        :param dbPath: The filesystem path to the SQLite database file.
+        :type dbPath: Path
+        """
         self.dbPath: Path = dbPath
         self.engine: Engine = create_engine(url=f"sqlite:///{self.dbPath}")
         self.metadata: MetaData = MetaData()
 
     def createTables(self) -> None:
+        """
+        Creates the database tables based on the defined metadata.
+
+        Defines tables for storing documents and categories with appropriate
+        relationships and constraints. This method checks first whether
+        the tables already exist before creating them to avoid duplication.
+
+        The tables defined are:
+        - documents: Contains columns like id, authors, submitter, title, etc.
+        - categories: Contains columns like id, arxiv_id (linked to documents),
+            and category.
+        """
         _: Table = Table(
             "documents",
             self.metadata,
