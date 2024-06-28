291c509575972c2c4ed872340d4f4ce8fbb34921
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-28 13:03:16 -0500

    Add code to read JSON Lines data

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-def main() -> None:
-    pass
+from pathlib import Path
+
+import pandas
+from pandas import DataFrame
+from pandas.io.json._json import JsonReader
+from progress.spinner import Spinner
+
+
+def main(inputPath: Path, chunksize: int) -> None:
+    absInputPath: Path = inputPath
+
+    dfs: JsonReader[DataFrame] = pandas.read_json(
+        path_or_buf=absInputPath, lines=True, chunksize=chunksize
+    )
+
+    with Spinner(message="Writing data to database...") as spinner:
+        for df in dfs:
+            print(df.columns)
+            spinner.next()
+            break
 
 
 if __name__ == "__main__":
-    main()
+    main(inputPath=Path("../data/arxiv.jsonlines"), chunksize=10000)
