2d6050706bd8c588854c43a45fb14a29c8380ada
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-06-28 12:32:49 -0500

    Add script to download the arxiv dataset

--- scripts/download.bash
+++ scripts/download.bash
@@ -1,9 +1,22 @@
 #!/bin/bash
 source optparse.bash
 
-optparse.define short=f long=file desc="The file to process" variable=file
-optparse.define short=o long=output desc="The output file" variable=output default=head_output.txt
-optparse.define short=l long=lines desc="The number of lines to head (default:5)" variable=lines default=5
-optparse.define short=v long=verbose desc="Flag to set verbose mode on" variable=verbose_mode value=true default=false
+optparse.define short=a long=kaggle-api-key desc="Kaggle API key" variable=KAGGLE_KEY
+optparse.define short=o long=output desc="Directory to save the file to" variable=outputDir default=../data
+optparse.define short=u long=kaggle-username desc="Kaggle username" variable=KAGGLE_USERNAME
 
 source $( optparse.build )
+
+if [[ -z $KAGGLE_USERNAME ]]; then
+    echo "No Kaggle username provided."
+    exit 1
+fi
+
+if [[ -z $KAGGLE_KEY ]]; then
+    echo "No Kaggle API key provided."
+    exit 1
+fi
+
+absOutputDir=$(realpath $outputDir)
+
+kaggle datasets download -p $absOutputDir --unzip -d Cornell-University/arxiv
